7fc4d00293c47efa63644cd44963d58c44d492fe
Your Name <you@example.com>
2019-07-15 14:47:38 -0400

    Updates

--- precache-manifest.57009f0c88f6caf93a4d4ddcc99928b5.js
+++ precache-manifest.57009f0c88f6caf93a4d4ddcc99928b5.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "94ce48328ad919f0cf613f0ab46d07cc",
+    "revision": "e853b6b97df4f9e53e7e1fd847b516ea",
     "url": "/tv-series-ratings-app/index.html"
   },
   {
-    "revision": "1cdd16665f8d5da8b88b",
+    "revision": "5a66d4e9f682d007a68d",
     "url": "/tv-series-ratings-app/static/css/main.6d3c6e55.chunk.css"
   },
   {
-    "revision": "60b23ea829c73fc80985",
-    "url": "/tv-series-ratings-app/static/js/2.a509bd87.chunk.js"
+    "revision": "bc6373bf612539361a78",
+    "url": "/tv-series-ratings-app/static/js/2.9f79d75d.chunk.js"
   },
   {
-    "revision": "1cdd16665f8d5da8b88b",
-    "url": "/tv-series-ratings-app/static/js/main.f11e01e5.chunk.js"
+    "revision": "5a66d4e9f682d007a68d",
+    "url": "/tv-series-ratings-app/static/js/main.22f806ff.chunk.js"
   },
   {
     "revision": "24900015f6a23b55e6aa",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tv-series-ratings-app/precache-manifest.33602f3991273c9b303379742a176648.js"
+  "/tv-series-ratings-app/precache-manifest.57009f0c88f6caf93a4d4ddcc99928b5.js"
 );
 
 self.addEventListener('message', (event) => {
