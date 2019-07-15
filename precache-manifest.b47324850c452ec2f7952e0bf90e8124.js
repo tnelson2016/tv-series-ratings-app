a2625d55c0449518bacbd160f6bfd663f423ee4b
Your Name <you@example.com>
2019-07-15 01:44:07 -0400

    Updates

---
+++ precache-manifest.b47324850c452ec2f7952e0bf90e8124.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c44474206b356fc1be443eb3a877b706",
+    "url": "/tv-series-ratings-app/index.html"
+  },
+  {
+    "revision": "5a66d4e9f682d007a68d",
+    "url": "/tv-series-ratings-app/static/css/main.6d3c6e55.chunk.css"
+  },
+  {
+    "revision": "bc6373bf612539361a78",
+    "url": "/tv-series-ratings-app/static/js/2.9f79d75d.chunk.js"
+  },
+  {
+    "revision": "5a66d4e9f682d007a68d",
+    "url": "/tv-series-ratings-app/static/js/main.22f806ff.chunk.js"
+  },
+  {
+    "revision": "24900015f6a23b55e6aa",
+    "url": "/tv-series-ratings-app/static/js/runtime~main.661f5021.js"
+  }
+]);
